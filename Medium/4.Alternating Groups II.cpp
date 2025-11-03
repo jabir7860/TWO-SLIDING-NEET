73fe6cf9cc47f632c3e1f4395966bc9a997f742a
Mohammad Jabir ☑️ <79660697+jabir7860@users.noreply.github.com>
2025-11-03 21:18:00 +0530

    Create 4.Alternating Groups II.cpp

---
+++ Medium/4.Alternating Groups II.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int numberOfAlternatingGroups(vector<int>& colors, int k) {
+        int ans=0; 
+        int maxLen=1; 
+        int n=colors.size(); 
+        for(int i=1;i<=n-1+k-1;i++){
+            if(colors[i%n]!=colors[(i-1+n)%n]){
+                maxLen++; 
+            }
+            else{
+                maxLen=1; 
+            }
+            if(maxLen>=k) ans++; 
+        }
+        return ans; 
+    }
+};
+
+TC : O(N) 
+SC : O(1) 

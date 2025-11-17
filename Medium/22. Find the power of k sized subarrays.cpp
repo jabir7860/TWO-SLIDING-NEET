453574b838620dbbee720fa7bc021a3db723fe0f
Mohammad Jabir ☑️ <79660697+jabir7860@users.noreply.github.com>
2025-11-17 14:29:28 +0530

    Create 22. Find the power of k sized subarrays.cpp

---
+++ Medium/22. Find the power of k sized subarrays.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    vector<int> resultsArray(vector<int>& nums, int k) {
+        int l=0,r=0,n=nums.size(); 
+        vector<int>ans; 
+        while(r<n){
+            while(r-l+1>k){
+                l++; 
+            }
+            if(r-l+1==k)
+            {
+              vector<int> newArr(nums.begin() + (l), nums.begin() + r+1); 
+              // sort(newArr.begin(),newArr.end()); 
+              // cout<<l<<" "<<r<<endl; 
+              // for(auto i:newArr) cout<<i<<" "; 
+              // cout<<endl; 
+              bool f=0; 
+              for(int i=1;i<newArr.size();i++){
+                if(newArr[i-1]!=newArr[i]-1)
+                {
+                    f=1; 
+                }
+              }
+              if(f==0){
+                ans.push_back(newArr[newArr.size()-1]); 
+              }
+              else ans.push_back(-1); 
+            }
+            r++; 
+        }
+        return ans; 
+    }
+};
+TC : O(N*K) where k is size of internal subarray that is formed; 
+SC : O(N) 
